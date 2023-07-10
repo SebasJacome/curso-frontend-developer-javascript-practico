3f21b47ae82cd85ea535dfa6bd3f43a3308943e3
SebasJacome <0237105@up.mx>
2023-07-10 16:36:20 -0600

    Making the mobile menu merge to the index

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
-const destkopMenu = document.querySelector('.desktop-menu')
+const destkopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuIcon = document.querySelector('.menu');
 
-menuEmail.addEventListener('click', toggleProfileMenu);
+menuEmail.addEventListener('click', toggleDestkopProfileMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
 
-function toggleProfileMenu(){
+function toggleDestkopProfileMenu(){
     destkopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
